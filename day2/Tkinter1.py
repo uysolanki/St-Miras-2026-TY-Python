602d4394e72c9cf33fe361faef2a1ffa94162bf3
uysolanki <uysolanki@gmail.com>
2026-08-08 09:54:13 +0530

    Tkinter prog 1

---
+++ day2/Tkinter1.py
@@ -0,0 +1,13 @@
+from tkinter import *
+root = Tk()
+root.geometry("500x200")
+root.title("Login Form")
+root.configure(bg="red")
+label1 = Label(root, text="Enter Name")
+label1.pack()
+
+label2 = Label(root, text="Enter Password", font=("Arial", 18, "bold"),fg="blue")
+label2.pack(pady=10)
+root.mainloop()
+
+
